24cd72c3884fdee91c2f311390f71a1c3f2c9757
Baltazar <130766615+Baltazar-Dostatny@users.noreply.github.com>
2024-02-07 15:23:04 -0300

    Delete server directory

--- server/CRUD.js
+++
@@ -1,9 +0,0 @@
-import { app, direotioPublic } from "./server.js";
-
-app.get("/", (req, res) => {
-    res.status(200).sendFile(`${direotioPublic}/pages/home/`);
-})
-
-app.get("/teste", (req, res) => {
-    res.status(200).sendFile(`${direotioPublic}/pages/teste/`);
-});
